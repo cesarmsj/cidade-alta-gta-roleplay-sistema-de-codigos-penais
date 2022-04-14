d8e1cd2e8fdda0d7c1b00f4cbe2e446e3e70d6e3
cesarmsj <cesarmsj@DESKTOP-ET0O42V>
2022-04-14 14:28:03 -0300

    Adicionar arquivos de projeto.

---
+++ cidade_alta_criminal_code/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cidade_alta_criminal_code.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
